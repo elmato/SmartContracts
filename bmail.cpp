3629273a48a3ae6d3997d30a7284cb3b723db149
Mário Silva <mmcs@outlook.pt>
2018-04-17 19:03:38 +0100

    Update bmail.cpp

--- bmail.cpp
+++ bmail.cpp
@@ -10,7 +10,7 @@ namespace eosio {
 	
 void bmail::sendmail( account_name from, 
                       account_name to,
-                      ipfshash_t   mailhash ) 
+                      string   mailhash ) 
 {
     print( "sendmail" );
     require_auth( from );
@@ -18,13 +18,12 @@ void bmail::sendmail( account_name from,
     add_mail(to, mailhash, false, from);
 }
 
-void bmail::add_mail( account_name owner, ipfshash_t mailhash, bool is_sender, account_name ram_payer )
+void bmail::add_mail( account_name owner, string mailhash, bool is_sender, account_name ram_payer )
 {
    mails to_mails( _self, owner );
    to_mails.emplace( ram_payer, [&]( auto& a ){
-      //for (int i=0; i < 34; i++)
-      //    a.mailhash[i] = mailhash[i];
-      a.mailhash = mailhash; 
+      for (int i=0; i < 34; i++)
+          a.mailhash[i] = mailhash[i];
       a.is_sender = is_sender;
    });
 }
